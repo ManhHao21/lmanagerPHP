4d764d7a98ad2bc5c327b9a8e89b9e3df8af05c4
haopm <haopm@sankei-bc.co.jp>
2025-05-22 07:34:59 +0700

    update

--- view/layouts/navLeft.blade.php
+++ view/layouts/navLeft.blade.php
@@ -7,21 +7,26 @@
                         Menu
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Quản lý người dùng <span class="badge badge-success">6</span></a>
+                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false"
+                            data-target="#submenu-1" aria-controls="submenu-1"><i
+                                class="fa fa-fw fa-user-circle"></i>Quản lý người dùng <span
+                                class="badge badge-success">6</span></a>
                         <div id="submenu-1" class="collapse submenu" style="">
                             <ul class="nav flex-column">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Danh sách</a>
+                                    <a class="nav-link" href="/admin/user">Danh sách</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Thêm</a>
+                                    <a class="nav-link" href="/admin/user/create">Thêm</a>
                                 </li>
                             </ul>
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2">
-                            <i class="fa fa-fw fa-list-alt"></i>Quản lý Danh mục <span class="badge badge-success">6</span>
+                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false"
+                            data-target="#submenu-2" aria-controls="submenu-2">
+                            <i class="fa fa-fw fa-list-alt"></i>Quản lý Danh mục <span
+                                class="badge badge-success">6</span>
                         </a>
                         <div id="submenu-2" class="collapse submenu">
                             <ul class="nav flex-column">
@@ -35,7 +40,8 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3" aria-controls="submenu-3">
+                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false"
+                            data-target="#submenu-3" aria-controls="submenu-3">
                             <i class="fa fa-fw fa-box"></i>Quản lý Sản phẩm <span class="badge badge-success">6</span>
                         </a>
                         <div id="submenu-3" class="collapse submenu">
